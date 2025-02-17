a27913d1aad5ec993b6fe660932d173b88e6aa4e
Alexsander <compjkee@gmail.com>
2025-02-17 15:12:11 +0200

    change response

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -5,15 +5,11 @@ exports.handler = async (event) => {
 	const method = event.requestContext.http.method;
 
 	if (path === '/hello' && method === 'GET') {
-		return {
-			body: JSON.stringify({ statusCode: 200, message: 'Hello from Lambda' }),
-		};
+		return JSON.stringify({ statusCode: 200, message: 'Hello from Lambda' });
 	} else {
-		return {
-			body: JSON.stringify({
-				statusCode: 400,
-				message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`,
-			}),
-		};
+		return JSON.stringify({
+			statusCode: 400,
+			message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`,
+		});
 	}
 };
